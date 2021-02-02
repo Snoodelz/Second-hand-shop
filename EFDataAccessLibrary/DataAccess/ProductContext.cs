68cb8e0c112f80546f1b7f6d5ed8fdc05fc6a39b
Ludwig Moström <ludwig.mostrom@gmail.com>
2021-02-02 17:06:28 +0100

    La till EFCore i projektet.

---
+++ EFDataAccessLibrary/DataAccess/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using EFDataAccessLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFDataAccessLibrary.DataAccess
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions options) : base(options) {}
+        public DbSet<Pants> Pants { get; set; }
+    }
+}
