eac682eae11b96d71eac08c028e1940ed643d805
Royale95 <rodi.yalcin@iths.se>
2021-02-03 13:14:52 +0100

    Admin Login Update

---
+++ SecondHandWebShop/Pages/AdminView.cshtml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SecondHandWebShop.Pages
+{
+    public class AdminViewModel : PageModel
+    {
+        public string UserName { get; set; }
+        public void OnGet()
+        {
+            UserName = HttpContext.Session.GetString("username");
+        }
+
+
+
+        public IActionResult OnGetLogout()
+        {
+            HttpContext.Session.Remove("username");
+            return RedirectToPage("/Login");
+        }
+    }
+}
