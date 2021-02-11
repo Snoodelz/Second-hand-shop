51c28488a0601936e0dd160de2ee4fadced17f28
Ludwig Moström <ludwig.mostrom@gmail.com>
2021-02-11 15:20:34 +0100

    La till utvalda produkter på startsidan och även en katalog sida som visar alla produkter.

--- SecondHandWebShop/Pages/Catalog.cshtml.cs
+++ SecondHandWebShop/Pages/Catalog.cshtml.cs
@@ -9,23 +9,33 @@ using System.Threading.Tasks;
 
 namespace SecondHandWebShop.Pages
 {
-    public class IndexModel : PageModel
+    public class CatalogModel : PageModel
     {
         private readonly EFDataAccessLibrary.DataAccess.ProductContext _context;
 
-        public IndexModel(EFDataAccessLibrary.DataAccess.ProductContext context)
+        public CatalogModel(EFDataAccessLibrary.DataAccess.ProductContext context)
         {
             _context = context;
         }
+
+        [BindProperty(SupportsGet = true)]
+        public string Category { get; set; }
         
         public List<Hat> Hats { get; set; }
         public List<Jacket> Jackets { get; set; }
+        public List<Jewelry> Jewelries { get; set; }
+        public List<Shirt> Shirts { get; set; }
+        public List<Shoe> Shoes { get; set; }
+        public List<Trousers> Trousers { get; set; }
 
         public void OnGet()
         {
             Hats = _context.Hats.ToList();
             Jackets = _context.Jackets.ToList();
-
+            Jewelries = _context.Jewelries.ToList();
+            Shirts = _context.Shirts.ToList();
+            Shoes = _context.Shoes.ToList();
+            Trousers = _context.Trousers.ToList();
         }
     }
 }
