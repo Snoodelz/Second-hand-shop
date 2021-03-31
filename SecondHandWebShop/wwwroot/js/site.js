d218fad05eab89dd28e7d2f71b9e2040a8e25e21
Ludwig Moström <ludwig.mostrom@gmail.com>
2021-03-31 13:01:41 +0200

    Design

--- SecondHandWebShop/wwwroot/js/site.js
+++ SecondHandWebShop/wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+$(document).ready(function () {
+    $('li.active').removeClass('active');
+    $('a[href="' + location.pathname + '"]').closest('li').addClass('active');
+});
