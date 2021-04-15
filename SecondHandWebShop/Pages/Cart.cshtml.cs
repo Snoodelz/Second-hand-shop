f27aa27faef6887ad2c550045ebc4b702db03dcc
Dyyal <71106959+Dyyal@users.noreply.github.com>
2021-04-15 14:38:25 +0200

    Merchandise bugfix
    Gick att lägga till fler produkter än det som finns på lager. Fixat nu.

--- SecondHandWebShop/Pages/Cart.cshtml.cs
+++ SecondHandWebShop/Pages/Cart.cshtml.cs
@@ -74,7 +74,7 @@ namespace SecondHandWebShop.Pages
                 }
                 else
                 {
-                    if (cart[index].Clothes.Category == "Merchandise")
+                    if (cart[index].Clothes.Category == "Merchandise" && cart[index].Clothes.StockBalance !> cart[index].Clothes.StockBalance)
                     {
                         cart[index].Quantity++;
                     }
